1c0324e0d3f335f50f7f82eceeb1beabea18015e
KangSeokJeon <studyandinvestigate@gmail.com>
2022-01-24 23:24:58 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native-web";
+import { View, Text } from "react-native";
+import { viewStyles, textStyles } from "./style";
 
 const App = () => {
   return (
-    <View styles={styles.container}>
-      <Text styles={styles.text}>Inline Styling - Text</Text>
-      <Text styles={styles.error}>Inline Styling - Error</Text>
+    <View styles={viewStyles.container}>
+      <Text styles={[textStyles.text, { color: "green" }]}>
+        Inline Styling - Text
+      </Text>
+      <Text styles={[textStyles.text, textStyles.error]}>
+        Inline Styling - Error
+      </Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    padding: 10,
-    fontSize: 26,
-    fontWeight: "600",
-    color: "black",
-  },
-  error: {
-    padding: 10,
-    fontSize: 26,
-    fontWeight: "400",
-    color: "red",
-  },
-});
 
 export default App;
